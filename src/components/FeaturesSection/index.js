7171be270b38b88ea0251ea423f7d7022852cf01
varshik <bunny6111998@gmail.com>
2022-05-11 16:34:00 +0000

    adding context example code

---
+++ src/components/FeaturesSection/index.js
@@ -0,0 +1,14 @@
+import Playtime from '../Playtime'
+import NewWaysToConnect from '../NewWaysToConnect'
+
+import './index.css'
+
+const FeaturesSection = () => (
+  //   const {activeLanguage} = props
+  <div className="features-section-container">
+    <Playtime />
+    <NewWaysToConnect />
+  </div>
+)
+
+export default FeaturesSection
